fcb23016137d5ba38c78f6ecef92bfbfedb5bad8
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-04-06 22:55:01 -0300

    Read keypress from the user

--- keto.c
+++ keto.c
@@ -1,3 +1,10 @@
+#include <unistd.h>
+
 int main() {
+    char c;
+    
+    /* Reads 1 byte of the standard input, and put it into the variable c, until there's 
+    no more bytes to read.*/
+    while(read(STDIN_FILENO, &c, 1) == 1)
     return 0;
 }
