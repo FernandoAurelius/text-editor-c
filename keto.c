14194a6f6b17ea2affd472cb68179df753402bc9
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-04-07 18:49:41 -0300

    Turn off output processing

--- keto.c
+++ keto.c
@@ -4,10 +4,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+// Global variable storing the original flags of the terminal.
 struct termios orig_termios;
 
 /* Disable the modifications made by enableRawMode() returning the flags modifieds 
-* to its original state using a variable "orig_termios" 
+* to its original state using a variable "orig_termios".
 */
 void disableRawMode() {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
@@ -29,6 +30,8 @@ void enableRawMode() {
     
     // Disable CTRL-S, CTRL-Q, fix CTRL-M in terminal.
     raw.c_iflag &= ~(IXON | ICRNL);
+    // Disable output process of newline (\r\n).
+    raw.c_oflag &= ~(OPOST);
     // Disable ECHO, CTRL-C, CTRL-Z, CTRL-V and CTRL-O in terminal.
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
@@ -46,13 +49,13 @@ int main() {
     * Now, the program will 'quit' after the user enters the letter 'q'.
     */
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-		// In case it's a non-printable character, just shows its ASCII code
+		// In case it's a non-printable character, just shows its ASCII code.
 		if (iscntrl(c)) {
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		} else {
 			// In case it's a printable character, shows its ASCII code and its 
-			// character representation
-			printf("%d ('%c')\n", c, c);
+			// character representation.
+			printf("%d ('%c')\r\n", c, c);
 		}
     }
     return 0;
