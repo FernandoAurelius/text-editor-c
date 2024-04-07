b93822b0a50c7c9c213c341052f6bbcba38beff1
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-04-06 23:27:41 -0300

    Add quit command 'q'

--- keto.c
+++ keto.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main() {
     char c;
     
     /* Reads 1 byte of the standard input, and put it into the variable c, until there's 
-    no more bytes to read.*/
-    while(read(STDIN_FILENO, &c, 1) == 1)
+    * no more bytes to read. 
+    *
+    * Now, the program will 'quit' afther the user enters the letter 'q'.
+    */
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
